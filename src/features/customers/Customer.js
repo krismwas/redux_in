aa9991b94d19f3777f5f880bc1f99892b7248c5f
kris <krismwasgit@gmail.com>
2025-01-24 18:44:01 +0300

    creating a customer

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -4,7 +4,7 @@ function Customer() {
   const customer = useSelector((store) => store.customer.fullname);
 
   console.log(customer);
-  return <h2>👋 Welcome, %NAME%</h2>;
+  return <h2>👋 Welcome, {customer}</h2>;
 }
 
 export default Customer;
